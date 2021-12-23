468d6789d99199b8cab4fb56345aaaccbd141246
Shaya Selincourt <sselincourt@gmail.com>
2021-12-22 23:05:22 -0800

    Set Swagger docs to root

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,13 +6,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('RandomCats Documention')
-    .setDescription('RandomCats API description')
+    .setTitle('RandomCats Documentation')
+    .setDescription('RandomCats API documentation')
     .setVersion('1.0')
     .addTag('cats')
     .build();
+  const options = {
+    customCss: '.swagger-ui .topbar { display: none }',
+  };
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document, options);
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 
